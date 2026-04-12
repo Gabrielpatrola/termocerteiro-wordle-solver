761720d5a0dbd9148641b70cd14543a2fc7a355c
Gabriel Almeida <gabriel@patrola.tech>
2026-04-12 17:29:46 -0400

    feat(backend): Add additional allowed origins for CORS configuration

--- backend/main.py
+++ backend/main.py
@@ -41,6 +41,8 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "http://127.0.0.1:5173",
+        "https://termocerteiro.com.br",
+        "https://patrola.me"
     ],
     allow_credentials=True,
     allow_methods=["*"],
