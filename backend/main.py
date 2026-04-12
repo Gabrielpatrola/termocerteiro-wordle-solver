7bc2c29b6d63c368ad303278ce0f0d34b8b07ee8
Gabriel Almeida <gabriel@patrola.tech>
2026-04-12 17:37:49 -0400

    feat(backend): Add new origin to CORS configuration for enhanced API access

--- backend/main.py
+++ backend/main.py
@@ -42,7 +42,8 @@ app.add_middleware(
         "http://localhost:5173",
         "http://127.0.0.1:5173",
         "https://termocerteiro.com.br",
-        "https://patrola.me"
+        "https://patrola.me",
+        "https://sea-lion-app-8wt77.ondigitalocean.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
